3eb045f47c5938476aa6047cb03486993bfd6d09
Aravindh <aravindhprabu2005@gmail.com>
2025-12-16 20:38:45 +0530

    lambda migration

--- client/src/axiosInstance.js
+++ client/src/axiosInstance.js
@@ -1,11 +1,11 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: 'https://ebwvrxp1jg.execute-api.eu-north-1.amazonaws.com/dev',
+  baseURL: 'https://0azyu9zvog.execute-api.ap-south-1.amazonaws.com/dev',
 });
 
 
 export default axiosInstance;
 
-// https://ebwvrxp1jg.execute-api.eu-north-1.amazonaws.com/dev
+// https://0azyu9zvog.execute-api.ap-south-1.amazonaws.com/dev
 // http://localhost:8000
