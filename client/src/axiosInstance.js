062e9d09dfbdcb31de1efa3148808ef958fc5f9c
Aravindh <aravindhprabu2005@gmail.com>
2025-12-18 12:49:21 +0530

    connected with EC2

--- client/src/axiosInstance.js
+++ client/src/axiosInstance.js
@@ -1,11 +1,11 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: 'http://localhost:8000',
+  baseURL: 'https://psycare.aravindhprabu.me/',
 });
 
 
 export default axiosInstance;
 
-// https://0azyu9zvog.execute-api.ap-south-1.amazonaws.com/dev
+// https://psycare.aravindhprabu.me/
 // http://localhost:8000
