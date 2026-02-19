70fbda0e26eeb8656f80d00659edb56f5ce579f8
Aravindh <aravindhprabu2005@gmail.com>
2026-02-19 23:15:14 +0530

    adding axios instance

---
+++ client/src/axiosInstance.js
@@ -0,0 +1,11 @@
+import axios from 'axios';
+
+const axiosInstance = axios.create({
+  baseURL: 'https://psycare.aravindhprabu.me/',
+});
+
+
+export default axiosInstance;
+
+// https://psycare.aravindhprabu.me/
+// http://localhost:8000
